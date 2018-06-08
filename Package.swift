1741252860647c33a9e9c73c93d29b8ba06ed38c
Халяпин Илья <962426@mail.ru>
2018-06-08 18:48:25 +0300

    Обновлен Vapor

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "MyPlaces",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.4"),
 
         // 🖋🐬 Swift ORM (queries, models, relations, etc) built on MySQL.
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2.5"),
