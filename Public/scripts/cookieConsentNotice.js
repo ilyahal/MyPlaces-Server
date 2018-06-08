b42fee8d47bcc3c8060b65ee056738c68fde1da6
Халяпин Илья <962426@mail.ru>
2018-06-08 23:08:09 +0300

    Реализовано отображение расширенной информации о куки

--- Public/scripts/cookieConsentNotice.js
+++ Public/scripts/cookieConsentNotice.js
@@ -1,9 +1,13 @@
 function cookiesConfirmed() {
-    $('#cookieFooter').hide();
+    cookiesClosed();
 
     var date = new Date();
     date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000)); // 1 год
 
     var expires = "expires="+ date.toUTCString();
     document.cookie = "cookies-accepted=true;" + expires;
+}
+
+function cookiesClosed() {
+    $('#cookieFooter').hide();
 }
